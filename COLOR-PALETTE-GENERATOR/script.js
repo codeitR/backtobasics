04e0f18fbebf8cc2212ae442cbeed8f2a6e9b622
Ramesh <90155745+codeitR@users.noreply.github.com>
2025-07-06 20:25:50 +0200

    update on eventlistner on palettecontainer

--- COLOR-PALETTE-GENERATOR/script.js
+++ COLOR-PALETTE-GENERATOR/script.js
@@ -1,7 +1,36 @@
 const generateBtn = document.getElementById("generate-btn");
 const paletteContainer = document.querySelector(".palette-container");
 
-generateBtn.addEventListener("click", generatePalette)
+generateBtn.addEventListener("click", generatePalette);
+
+paletteContainer.addEventListener("click", function(e){
+  if(e.target.classList.contains("copy-btn")){
+    const hexValue = e.target.previousElementSibling.textContent;
+    navigator.clipboard.writeText(hexValue)
+    .then(() => showCopySuccess(e.target))
+    .catch((err) => console.log(err));
+} else if (e.target.classList.contains("color")) {
+  const hexValue = e.target.nextElementSibling.querySelector(".hex-value").textContent;
+  navigator.clipboard
+    .writeText(hexValue)
+    .then(() => showCopySuccess(e.target.nextElementSibling.querySelector(".copy-btn")))
+    .catch((err) => console.log(err));
+}
+});
+
+function showCopySuccess(element) {
+  element.classList.remove("far", "fa-copy");
+  element.classList.add("fas", "fa-check");
+
+  element.style.color = "#48bb78";
+
+  setTimeout(() => {
+    element.classList.remove("fas", "fa-check");
+    element.classList.add("far", "fa-copy");
+    element.style.color = "";
+  }, 1000);
+}
+
 
 function generatePalette(){
   const colors = [];
@@ -18,7 +47,7 @@ function getRandomColor() {
   }
   return color;
 }
-generatePalette()
+
 
 function updatePletteDisplay(colors) {
   const colorBoxes = document.querySelectorAll(".color-box");
@@ -30,3 +59,4 @@ function updatePletteDisplay(colors) {
     hexValue.textContent = color;
   });
 }
+// generatePalette()
